93d60be264e5f8ee5f416e6f648996e5d6a19ffa
haiwang <haiwang.li@newtranx.com>
2017-02-22 11:20:27 +0800

    创建新测试工程

---
+++ TestModuleDev1/TestModuleDev1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestModuleDev1
+//
+//  Created by haiwang on 2017/2/22.
+//  Copyright © 2017年 HAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestModuleDev1/TestModuleDev1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestModuleDev1
+//
+//  Created by haiwang on 2017/2/22.
+//  Copyright © 2017年 HAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
